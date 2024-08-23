803f9d0f8f144a0a89b030bf2ee99c92187d8da0
Zach Madsen <zach@zachmadsen.com>
2024-08-23 16:16:56 -0700

    Use clap instead of lexopt

--- crates/frontend/src/main.rs
+++ crates/frontend/src/main.rs
@@ -1,10 +1,32 @@
-use lexopt::prelude::*;
+use clap::{Parser, Subcommand, ValueEnum};
+
+#[derive(Debug, Parser)]
+#[command(version)]
+pub struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Set a display's input
+    Set {
+        /// The display name
+        display: String,
+        /// The input to switch to
+        #[arg(value_enum)]
+        input: Input,
+    },
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum Input {
+    DisplayPort1,
+    DisplayPort2,
+    Hdmi1,
+    Hdmi2,
+}
 
 fn main() {
-    let mut parser = lexopt::Parser::from_env();
-    let Ok(Some(Value(command))) = parser.next() else {
-        eprintln!("chmi: error: expected a command");
-        return;
-    };
-    println!("command: {}", command.to_str().unwrap());
+    let _args = Args::parse();
 }
